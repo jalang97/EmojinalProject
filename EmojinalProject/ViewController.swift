a0936a34b9fea9bc87f7df68888c94dc6bbd30cb
Julia Lang <julia.al.lang@gmail.com>
2021-05-25 16:30:14 -0700

    Initial Commit

---
+++ EmojinalProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmojinalProject
+//
+//  Created by Julia Lang on 5/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
