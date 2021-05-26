db81e3f7eef2b73405eb2dc5f31bd72ca32bf660
Julia Lang <julia.al.lang@gmail.com>
2021-05-25 17:25:43 -0700

    Complete Iteration 3.

--- EmojinalProject/ViewController.swift
+++ EmojinalProject/ViewController.swift
@@ -13,13 +13,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    let emojis = ["😃": "Happy", "😔": "Sad", "😡": "Angry"]
+    let emojis = ["😃": "Happy", "😢": "Sad", "😡": "Angry"]
+    let customMessages = ["Happy": ["woohoo! Have an awesome day!", "yeehaw!", "Good day sunshine!"], "Sad": ["cheer up buttercup", "take a deep breathe", "do you need to talk?"], "Angry": ["Go for a run and work out that anger!", "Take deep breaths", "Everything will be ok!!"]]
+    
     @IBAction func showMessage(sender: UIButton) {
-        let selectedEmotion = sender.titleLabel?.text
-        //in as much detail as possible
-        // pseudo-code the things that need to happen
-        // when a user clicks an emoji button, make a pop up message that changes
-        let alertController=UIAlertController(title: "title", message: selectedEmotion, preferredStyle: UIAlertController.Style.alert)
+        let selectedEmotion = (sender.titleLabel?.text)!
+        let options = customMessages[emojis[selectedEmotion]!]!
+        let randomNum = Int.random(in: 0...2)
+        let emojiMessage = options[randomNum]
+    
+        let alertController=UIAlertController(title: selectedEmotion, message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         
         present(alertController, animated: true, completion: nil)
